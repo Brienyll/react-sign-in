7e84a617b6f3066e03d70edf2a85c70b57fcc28a
Brienyll <Brienyll29@gmail.com>
2021-08-12 09:04:43 -0700

    first commit

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,14 @@
+
+const AddUser = props => {
+  return (
+    <form onSubmit={}>
+      <label htmlFor='username'>Username</label>
+      <input id='username' type='text' />
+      <label htmlFor='age'>Age</label>
+      <input id='age' type='number' />
+      <button type='submit'>Add User</button>
+    </form>
+  )
+};
+
+export default AddUser;
